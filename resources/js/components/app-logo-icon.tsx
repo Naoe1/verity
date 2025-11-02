f9af123654c1920888286bf5410d156a5ce2d783
Noea <gllnaoe@gmail.com>
2025-11-02 19:50:23 +0800

    Update favicon and app logo icon

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -2,11 +2,50 @@ import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
     return (
-        <svg {...props} viewBox="0 0 40 42" xmlns="http://www.w3.org/2000/svg">
+        <svg
+            width="60"
+            height="60"
+            viewBox="0 0 60 60"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect x="0" y="0" width="60" height="60" fill="transparent" />
+
+            <defs>
+                <linearGradient
+                    id="mainGradient"
+                    x1="15"
+                    y1="15"
+                    x2="45"
+                    y2="45"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stopColor="#0F2027" />
+                    <stop offset="1" stopColor="#2C5364" />
+                </linearGradient>
+
+                <linearGradient
+                    id="accentGradient"
+                    x1="20"
+                    y1="20"
+                    x2="50"
+                    y2="50"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stopColor="#3A7BD5" />
+                    <stop offset="1" stopColor="#00D2FF" />
+                </linearGradient>
+            </defs>
+
             <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M17.2 5.63325L8.6 0.855469L0 5.63325V32.1434L16.2 41.1434L32.4 32.1434V23.699L40 19.4767V9.85547L31.4 5.07769L22.8 9.85547V18.2999L17.2 21.411V5.63325ZM38 18.2999L32.4 21.411V15.2545L38 12.1434V18.2999ZM36.9409 10.4439L31.4 13.5221L25.8591 10.4439L31.4 7.36561L36.9409 10.4439ZM24.8 18.2999V12.1434L30.4 15.2545V21.411L24.8 18.2999ZM23.8 20.0323L29.3409 23.1105L16.2 30.411L10.6591 27.3328L23.8 20.0323ZM7.6 27.9212L15.2 32.1434V38.2999L2 30.9666V7.92116L7.6 11.0323V27.9212ZM8.6 9.29991L3.05913 6.22165L8.6 3.14339L14.1409 6.22165L8.6 9.29991ZM30.4 24.8101L17.2 32.1434V38.2999L30.4 30.9666V24.8101ZM9.6 11.0323L15.2 7.92117V22.5221L9.6 25.6333V11.0323Z"
+                d="M15 15L45 15L45 35L30 45L15 35L15 15Z"
+                fill="url(#mainGradient)"
+            />
+
+            <path
+                d="M20 25L50 25L50 45L35 55L20 45L20 25Z"
+                fill="url(#accentGradient)"
+                opacity="0.9"
             />
         </svg>
     );
